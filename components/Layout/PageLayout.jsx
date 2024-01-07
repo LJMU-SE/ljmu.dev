49204e54fe89d850d42b660ea114cf7e0b8513db
Ollie Beenham <ollie@beenhamow.co.uk>
2024-01-07 22:53:06 +0000

    Added page metadata

--- components/Layout/PageLayout.jsx
+++ components/Layout/PageLayout.jsx
@@ -2,17 +2,52 @@ import { Inter } from "next/font/google";
 import NavBar from "./Navbar";
 import Footer from "./Footer";
 import Head from "next/head";
+import { useRouter } from "next/router";
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Layout({ children, fixedNav = false, name }) {
+export const getServerSideProps = (context) => ({
+    props: { host: context.req.headers.host || null },
+});
+
+export default function Layout({
+    children,
+    host,
+    fixedNav = false,
+    name,
+    ...props
+}) {
+    const title = name
+        ? `${name} | LJMU Software Engineering Team`
+        : "LJMU Software Engineering Team";
+
+    const description = props.description || "No Description Provided";
+    const image =
+        props.image || "https://www.ljmu.dev/img/metadata/og-large.png";
+
+    const router = useRouter();
+    const url = `https://${host || "ljmu.dev"}${router.asPath}`;
+
     return (
         <div className={`${inter.className}`}>
             <Head>
-                <title>
-                    {name
-                        ? `${name} | LJMU Software Engineering Team`
-                        : "LJMU Software Engineering Team"}
-                </title>
+                {/* <!-- HTML Meta Tags --> */}
+                <title>{title}</title>
+                <meta name="description" content={description} />
+                {/* <!-- Google / Search Engine Tags --> */}
+                <meta itemProp="name" content={title} />
+                <meta itemProp="description" content={description} />
+                <meta itemProp="image" content={image} />
+                {/* <!-- Facebook Meta Tags --> */}
+                <meta property="og:url" content={url} />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
+                <meta property="og:image" content={image} />
+                {/* <!-- Twitter Meta Tags --> */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content={title} />
+                <meta name="twitter:description" content={description} />
+                <meta name="twitter:image" content={image} />
                 <link
                     rel="icon"
                     type="image/webp"
