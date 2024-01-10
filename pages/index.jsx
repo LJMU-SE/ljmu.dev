6340db4fa2d8004055bf9e90df28e2bb63497587
Ollie Beenham <ollie@beenhamow.co.uk>
2024-01-10 01:33:30 +0000

    Added video to about us section

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,8 +13,8 @@ export default function Home() {
         >
             <LandingHeader />
             <Container>
-                <div className="py-8 flex flex-col md:flex-row justify-center items-center">
-                    <div className="w-full px-0 md:pr-4">
+                <div className="py-8 flex flex-col lg:flex-row justify-center items-center">
+                    <div className="w-full px-0 lg:pr-4">
                         <h1 className="text-2xl">About Us</h1>
                         <p className="mt-3">
                             Lorem ipsum dolor sit amet, consectetur adipiscing
@@ -29,20 +29,12 @@ export default function Home() {
                             amet vitae mauris.
                         </p>
                     </div>
-                    <div className="w-full px-0 md:pl-4">
-                        <h1 className="text-2xl">About Us</h1>
-                        <p className="mt-3">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Ut ornare mauris a risus dignissim posuere.
-                            Donec massa orci, auctor non venenatis et, convallis
-                            ut ante. Morbi ut dapibus quam. Duis efficitur ante
-                            libero, et suscipit massa sagittis eget. Nunc
-                            egestas sem pharetra sapien semper, ac vulputate
-                            velit sollicitudin. Nam vel enim at quam dictum
-                            vestibulum ut laoreet arcu. Fusce dignissim porta
-                            risus. Fusce eu ante ut nisi tempor pulvinar sit
-                            amet vitae mauris.
-                        </p>
+                    <div className="w-full px-0 mt-4 lg:my-0 lg:pl-4">
+                        <video
+                            src="/video/landing.mp4"
+                            className="w-full rounded-lg"
+                            controls
+                        />
                     </div>
                 </div>
             </Container>
