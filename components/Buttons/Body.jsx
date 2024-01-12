32da02c94307070d16aa000e06e01595ecc160ad
Ollie Beenham <ollie@beenhamow.co.uk>
2024-01-12 19:47:24 +0000

    Started projects section

--- components/Buttons/Body.jsx
+++ components/Buttons/Body.jsx
@@ -1,14 +1,15 @@
-export default function BodyButton({
-    children,
-    onClick,
-    type = "button",
-    className,
-}) {
-    if (!className)
-        className =
-            "flex items-center justify-center w-max max-w-xs px-5 py-2 text-base text-center bg-black text-white rounded-md hover:opacity-70 transition-all";
+export default function BodyButton({ children, onClick, ...args }) {
+    const type = args.type || "button";
+    const width = args.width || "w-max";
+    const bg = args.bg || "bg-black";
+    const text = args.text || "text-white";
+
     return (
-        <button onClick={onClick} type={type} className={className}>
+        <button
+            onClick={onClick}
+            type={type}
+            className={`flex items-center justify-center ${width} px-5 py-2 text-base text-center ${bg} ${text} rounded-md hover:opacity-70 transition-all`}
+        >
             {children}
         </button>
     );
