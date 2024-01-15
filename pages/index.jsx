815141ff35aeecf39cc526ac3ae0f7ce260e6959
Ollie Beenham <ollie@beenhamow.co.uk>
2024-01-15 05:03:21 +0000

    test commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,6 +14,7 @@ export default function Home() {
         >
             <LandingHeader />
             <AboutSection />
+            <p>test commit</p>
             <ProjectsSection />
         </Layout>
     );
